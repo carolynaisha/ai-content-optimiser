107cbff6f7fe607f6f003f95c9911d34f4c65993
carolynaisha <aisha@carolynaisha.co.uk>
2025-08-22 15:23:44 +0100

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
-import './index.css'   // <- required for Tailwind
+import './index.css'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(<App />)
+
