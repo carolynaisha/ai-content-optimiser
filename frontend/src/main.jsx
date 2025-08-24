4e99aa4db4714416466848279a6b84f0c4baf2a3
carolynaisha <aisha@carolynaisha.co.uk>
2025-08-24 18:06:30 +0100

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,11 @@
+
+// 🚫 Disable any old service workers that might cache old builds
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then(regs =>
+    regs.forEach(r => r.unregister())
+  );
+}
+
 import './index.css'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
