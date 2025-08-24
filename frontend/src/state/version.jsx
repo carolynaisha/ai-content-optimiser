abdb485694cfe338226d7decf46c67d6228e2ef2
carolynaisha <aisha@carolynaisha.co.uk>
2025-08-24 18:11:32 +0100

    Update version.jsx

--- frontend/src/state/version.jsx
+++ frontend/src/state/version.jsx
@@ -1,1 +1,11 @@
+// src/state/version.js
+
+// bump this string whenever you make breaking frontend changes
+export const CLIENT_SCHEMA_VERSION = "v1";
+
+// clear old localStorage if version has changed
+if (localStorage.getItem("co_version") !== CLIENT_SCHEMA_VERSION) {
+  localStorage.clear();
+  localStorage.setItem("co_version", CLIENT_SCHEMA_VERSION);
+}
 
